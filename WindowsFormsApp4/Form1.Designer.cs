658ed48cbf4fa3a4cf93a2b4c4f401696b1d0ea8
Nurullah <nurlan3295@gmail.com>
2019-01-13 23:10:05 +0400

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Baku_Button = new System.Windows.Forms.Button();
+            this.London_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(211, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 194);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Baku_Button
+            // 
+            this.Baku_Button.Location = new System.Drawing.Point(187, 268);
+            this.Baku_Button.Name = "Baku_Button";
+            this.Baku_Button.Size = new System.Drawing.Size(75, 23);
+            this.Baku_Button.TabIndex = 1;
+            this.Baku_Button.Text = "Baku";
+            this.Baku_Button.UseVisualStyleBackColor = true;
+            this.Baku_Button.Click += new System.EventHandler(this.Baku_Button_Click);
+            // 
+            // London_Button
+            // 
+            this.London_Button.Location = new System.Drawing.Point(508, 268);
+            this.London_Button.Name = "London_Button";
+            this.London_Button.Size = new System.Drawing.Size(82, 23);
+            this.London_Button.TabIndex = 2;
+            this.London_Button.Text = "London";
+            this.London_Button.UseVisualStyleBackColor = true;
+            this.London_Button.Click += new System.EventHandler(this.London_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.London_Button);
+            this.Controls.Add(this.Baku_Button);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Baku_Button;
+        private System.Windows.Forms.Button London_Button;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public  bool Baku=false;
+        public  bool London = false;
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            if(Baku==true)
+            {
+                Image i = Image.FromFile("Baku.jpg");
+                label1.Image = i;
+                label1.Text = dateTime.ToString();
+            }
+            else
+            {
+                Image i = Image.FromFile("London.jpg");
+                label1.Image = i;
+                var a = dateTime.Hour + 4;
+                DateTime dateTime1 = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, a, dateTime.Minute, dateTime.Second);
+                label1.Text = dateTime1.ToString();
+            }
+        }
+        private void Baku_Button_Click(object sender, EventArgs e)
+        {
+            Baku = true;
+            London = false;    
+            Timer timer = new Timer();         
+            timer.Start();
+            timer.Tick += new EventHandler(label1_Click);
+            timer.Interval = 1000;          
+        }
+        private void London_Button_Click(object sender, EventArgs e)
+        {
+            Baku = false;  
+            London = true;          
+            Timer timer = new Timer();
+            timer.Start();
+            timer.Tick += new EventHandler(label1_Click);
+            timer.Interval = 1000;
+        }
+    }
+}
